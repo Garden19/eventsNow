ab44ff8f436575d11d9e23e84dd2ff65a0d03e16
EdanR <edanjack1996@hotmail.com>
2026-06-19 02:20:16 +0100

    Added a served landing page

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
+const path = require("path");
 
 const outdoorCinemas = require("./routes/outdoorCinemas");
 
 app.use("/outdoor-cinemas", outdoorCinemas);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log("Listening on: http://localhost:" + PORT + "/");
